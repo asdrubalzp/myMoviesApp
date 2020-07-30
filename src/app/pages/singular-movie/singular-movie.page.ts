51635be2dd62c1039b7493bd7515149dc38e4c28
Geovanny Zambrano <geovannya.zamp@hotmail.com>
2020-07-29 21:31:50 -0500

    interzaz de pelicula

--- src/app/pages/singular-movie/singular-movie.page.ts
+++ src/app/pages/singular-movie/singular-movie.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
 import { MovieService } from "../../services/movie.service";
+import { AlertController } from "@ionic/angular";
 
 @Component({
   selector: "app-singular-movie",
@@ -12,7 +13,8 @@ export class SingularMoviePage implements OnInit {
   movie: any;
   constructor(
     private activatedRoute: ActivatedRoute,
-    private movieService: MovieService
+    private movieService: MovieService,
+    public alertController: AlertController
   ) {
     this.idMovie = activatedRoute.snapshot.paramMap.get("id");
   }
@@ -21,6 +23,16 @@ export class SingularMoviePage implements OnInit {
     this.searchMovie();
   }
 
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      cssClass: "my-custom-class",
+      header: "Error",
+      message: "Esta funcionalidad aun está en proceso 😭",
+      buttons: ["OK"],
+    });
+
+    await alert.present();
+  }
   searchMovie() {
     // tslint:disable-next-line: radix
     this.movie = this.movieService.searchMovie(parseInt(this.idMovie));
